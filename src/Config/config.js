01fe63a6cc38ab39fd7fea0dbf7ad74b67194b43
tempiltin <temurbekshukurov0707@gmail.com>
2022-09-08 23:16:03 +0500

    upload

---
+++ src/Config/config.js
@@ -0,0 +1,14 @@
+import * as frebase from "firebase/app";
+import "firebase/auth";
+const firebaseConfig = {
+    apiKey: "AIzaSyAdiJ7rCwSs_rANMrj6sdcaT6YHkQG1QPk",
+    authDomain: "myportfolio-40fa1.firebaseapp.com",
+    projectId: "myportfolio-40fa1",
+    storageBucket: "myportfolio-40fa1.appspot.com",
+    messagingSenderId: "275745359408",
+    appId: "1:275745359408:web:28e98140251ac071300ff5",
+    measurementId: "G-L4DYVHJSH1"
+  };
+
+  export default config;
+  "https://www.youtube.com/watch?v=sMq6RDYqcm4"
